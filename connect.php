526d3217656fb83a559a25fa25d153c322c6c0b5
M.solution <97136008+MrSolution07@users.noreply.github.com>
2024-05-26 14:31:48 +0200

    new database

--- connect.php
+++ connect.php
@@ -1,17 +1,14 @@
+
 <?php
 
-    $servername = "jfrpocyduwfg38kq.chr7pe7iynqr.eu-west-1.rds.amazonaws.com";
-    $username = "uzbwbfu8fpa63ede";
-    $password = "bd8fhd378x2fuvdj";
-    $database = "n4xzjd0f0ks0aq5y";
-    $port = 3306;
+$servername = "10-5-6-196.proxysql-cluster-passive.proxysql.svc.cluster.local";
+$username = "b0280930e1e73e";
+$password = "7cf3a3dd72b3087"; 
+$database = "heroku_19e1b467e1a92b8"; // Fill in the database name
+$port = 3306; // Assuming default MySQL port
 
-    $conn = new mysqli($servername, $username, $password, $database, $port);
+$conn = new mysqli($servername, $username, $password, $database, $port);
 
-    if ($conn->connect_error) {
-        die("Connection failed: " . $conn->connect_error);
-    }
-
-    echo "Connected successfully";
-
-?>
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
