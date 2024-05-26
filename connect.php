9a15d182c97f238994dcf1c070a981f52152c58d
M.solution <97136008+MrSolution07@users.noreply.github.com>
2024-05-26 13:22:57 +0200

    connect update

--- connect.php
+++ connect.php
@@ -1,14 +1,17 @@
 <?php
 
-$servername = "localhost";
-$username = "root";
-$password = "";
-$database = "users";
+    $servername = "jfrpocyduwfg38kq.chr7pe7iynqr.eu-west-1.rds.amazonaws.com";
+    $username = "uzbwbfu8fpa63ede";
+    $password = "bd8fhd378x2fuvdj";
+    $database = "n4xzjd0f0ks0aq5y";
+    $port = 3306;
 
-$conn = new mysqli($servername, $username, $password, $database);
-if($conn->connect_error){
-    die("Connection failed: " . $conn->connect_error);
-}
- 
+    $conn = new mysqli($servername, $username, $password, $database, $port);
+
+    if ($conn->connect_error) {
+        die("Connection failed: " . $conn->connect_error);
+    }
+
+    echo "Connected successfully";
 
 ?>
