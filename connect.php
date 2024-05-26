d9135e488eb8f2d694c1bc1e6ea33e9bb0005345
M.solution <97136008+MrSolution07@users.noreply.github.com>
2024-05-26 14:44:28 +0200

    update

--- connect.php
+++ connect.php
@@ -1,14 +1,19 @@
-
 <?php
 
-$servername = "10-5-6-196.proxysql-cluster-passive.proxysql.svc.cluster.local";
-$username = "b0280930e1e73e";
-$password = "7cf3a3dd72b3087"; 
-$database = "heroku_19e1b467e1a92b8"; // Fill in the database name
+$url = "mysql://b0280930e1e73e:8864fad9@eu-cluster-west-01.k8s.cleardb.net/heroku_19e1b467e1a92b8";
+$url_parts = parse_url($url);
+
+$servername = $url_parts['host'];
+$username = $url_parts['user'];
+$password = $url_parts['pass'];
+$database = ltrim($url_parts['path'], '/');
+
 $port = 3306; // Assuming default MySQL port
 
 $conn = new mysqli($servername, $username, $password, $database, $port);
 
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
-}
+} 
+
+?>
